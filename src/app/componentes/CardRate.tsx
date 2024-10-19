82fab6d329897994640228dfe6d5778ab0ae3606
adriano ferreira sales <adrianosalescontato@gmail.com>
2024-10-19 17:46:34 -0400

    modificações do modelo

--- src/app/componentes/CardRate.tsx
+++ src/app/componentes/CardRate.tsx
@@ -6,12 +6,25 @@ export const CardRate = ({rate}: Props) => {
     if(rate > 5) rate = 5;
     if(rate < 0) rate = 0;
 
-    const rateInt = Math.floor(rate);
-    const stars = '😁'.repeat(rateInt) + '😶'.repeat(5-rateInt);
+    const tiposEmojis: any = ["😔", "🙂", "😄", "😀", "😁"];
+
+    let rateInt = Math.floor(rate);
+    let notaInt = rateInt;
+    if(rateInt > 4) rateInt = 4;
+    if(rateInt <= 0) rateInt = 0;
+    
+    const constAtualEmoji = tiposEmojis[rateInt];
+
+    if(notaInt == 0){
+        notaInt = 1;
+    } 
+    
+    const stars = (constAtualEmoji).repeat(notaInt) + '😶'.repeat(5-notaInt);
+    
 
     return(
         <div className="flex gap-3 p-5 bg-sky-900 rounded-md shadow-zinc-200 shadow-sm">
-            <div className="">Nota: {rateInt} - </div>
+            <div className="">Nota: {notaInt} - </div>
             <div>{stars}</div>
         </div>
     );
